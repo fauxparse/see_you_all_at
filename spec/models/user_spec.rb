58279efc59049c22c57a5401329533c606547da3
Matt Powell <fauxparse@gmail.com>
2015-10-04 23:34:47 +1300

    coverage for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  subject { user }
+  let(:user) { FactoryGirl.create(:user) }
+
   it { is_expected.to validate_presence_of(:name) }
+
+  describe "#requires_email_address?" do
+    subject { user.requires_email_address? }
+
+    context "when email address has been entered" do
+      it { is_expected.to be(false) }
+    end
+
+    context "when email address is missing" do
+      before { user.email = user.confirmation_sent_at = nil }
+
+      it { is_expected.to be(true) }
+    end
+  end
 end
