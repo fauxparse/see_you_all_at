130286094cfaed4ae39138bedb3b7e85374c1937
Matt Powell <matt.powell@powershop.co.nz>
2015-10-09 15:51:26 +1300

    test sortable position updating

--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -1,8 +1,32 @@
 require "rails_helper"
 
 RSpec.describe Package, type: :model do
-  subject { FactoryGirl.create(:package) }
+  subject { FactoryGirl.create(:package, event: event) }
+  let(:event) { FactoryGirl.create(:event) }
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).scoped_to(:event_id) }
+
+  context "with siblings" do
+    subject { event.packages.first }
+
+    before do
+      3.times { FactoryGirl.create(:package, event: event) }
+    end
+
+    it "auto-increments positions" do
+      expect(event.packages.map(&:position).sort).to eq([0, 1, 2])
+    end
+
+    context "when destroyed" do
+      before do
+        subject.destroy
+        event.reload
+      end
+
+      it "updates the other packages' positions" do
+        expect(event.packages.map(&:position).sort).to eq([0, 1])
+      end
+    end
+  end
 end
